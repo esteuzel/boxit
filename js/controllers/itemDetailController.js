c5cc26e59b21b17bbcd4b2bd90bac44751617e61
uzel <fabiouz@gmail.com>
2018-04-26 20:51:39 -0300

    addtocar

--- js/controllers/itemDetailController.js
+++ js/controllers/itemDetailController.js
@@ -5,6 +5,7 @@ angular
 
             var item = $stateParams.itemId;
             var currentIdItem = $stateParams.itemId;
+            var currentItemObject = null;
             $scope.showZise = false;
             $scope.ShowColor = false;
             $scope.showCombination = false;
@@ -31,7 +32,7 @@ angular
                    $scope.imgUrl = item.Item.Image.ImageUrl;
                    $scope.itemPrice = item.Item.Offers.Offer.OfferListing.Price.FormattedPrice;
                    amount = item.Item.Offers.Offer.OfferListing.Price.Amount;
-                   $scope.peso = Math.ceil(item.Item.Attributes.PackageDimensions.Weight / 100);
+                   $scope.peso = item.Item.Attributes.PackageDimensions.Weight == null ? 0 : Math.ceil(item.Item.Attributes.PackageDimensions.Weight / 100);
                    setItemData(item);
                    setItemVariation(item);
                }
@@ -47,6 +48,8 @@ angular
 
             function setItemData(item) {
                 currentIdItem = item.Item.ItemId;
+                currentItemObject = item;
+                console.log('setItemData currentItemObject',currentItemObject);
                 $scope.itemCode = currentIdItem;
                 $scope.userNotLogged = usrObj === undefined;
                 $scope.titulo = item.Item.Attributes.Title;
@@ -68,7 +71,7 @@ angular
                 $scope.total = numeral(( amount * $scope.cantidad) / 100).format('$0,0.00');
                 $scope.showMain = true;
                 $scope.loadMain = false;
-                $scope.peso = Math.ceil(item.Item.Attributes.PackageDimensions.Weight / 100);
+                $scope.peso = item.Item.Attributes.PackageDimensions.Weight == null ? 0 : Math.ceil(item.Item.Attributes.PackageDimensions.Weight / 100);
                 getCar();
             }
 
@@ -196,17 +199,17 @@ angular
                 itemadded = $scope.itemSelected;
                 console.log('addToCar itemadded',itemadded);
                 var args = {};
-                    args["IdCliente"] = userData.getData().IdCliente;
-                    args["ItemId"] = itemadded.ItemId;
+                    args["IdCliente"] = userData.getData().IdCliente;                    
+                    args["ItemId"] = currentItemObject.Item.ItemId;
                     args["OfferListingId"] = "";
-                    args["Price"] = itemadded.Offers.Offer.OfferListing.Price.Amount / 100;
+                    args["Price"] = currentItemObject.Item.Offers.Offer.OfferListing.Price.Amount / 100;
                     // PackageDimensions 
-                    args["Height"] = itemadded.Attributes.PackageDimensions.Height == null ? 0 : itemadded.Attributes.PackageDimensions.Height;
-                    args["Length"] = itemadded.Attributes.PackageDimensions.Length == null ? 0 : itemadded.Attributes.PackageDimensions.Length;
-                    args["Weight"] = itemadded.Attributes.PackageDimensions.Weight == null ? 0 : itemadded.Attributes.PackageDimensions.Weight;
-                    args["Width"] = itemadded.Attributes.PackageDimensions.Width == null ? 0 : itemadded.Attributes.PackageDimensions.Width;               
+                    args["Height"] = currentItemObject.Item.Attributes.PackageDimensions.Height == null ? 0 : currentItemObject.Item.Attributes.PackageDimensions.Height;
+                    args["Length"] = currentItemObject.Item.Attributes.PackageDimensions.Length == null ? 0 : currentItemObject.Item.Attributes.PackageDimensions.Length;
+                    args["Weight"] = currentItemObject.Item.Attributes.PackageDimensions.Weight == null ? 0 : currentItemObject.Item.Attributes.PackageDimensions.Weight;
+                    args["Width"] = currentItemObject.Item.Attributes.PackageDimensions.Width == null ? 0 : currentItemObject.Item.Attributes.PackageDimensions.Width;               
                     // Image
-                    args["UrlImage"] = itemadded.Image.ImageUrl;
+                    args["UrlImage"] = currentItemObject.Item.Image.ImageUrl;
                     console.log("args",args);
 
                 if ($scope.cantidad == 0 || $scope.cantidad === undefined) {
@@ -318,97 +321,5 @@ angular
                 $('.navbar').removeClass('white');
             });            
             $('.navbar').addClass('white');
-            
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-            console.log(item);
-            var amount;
-            var carItemId;
-            var getQuantity = function (carItems) {
-                if ( carItems.length === undefined) {
-                     console.log(carItems);
-                     return carItems.Quantity;
-                }
-                for (var i = 0; i < carItems.length; i++) {
-                    var item = carItems[i];
-                    if (item.ItemId == $stateParams.itemId) {
-                        carItemId = item.CartItemId;
-                        return item.Quantity;
-                    }
-                }
-            };
-
-            $scope.modifyCar = function () {
-                var args = {};
-                args["IdCliente"] = userData.getData().IdCliente;
-                args["CartItemId"] = carItemId;
-                args["Quantity"] = $scope.cantidad;
-                $http({
-                    method: "POST",
-                    url: userData.getHost() + "/amazon/amazonmodifycart",
-                    data: args,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then(function success(result) {
-                    //$scope.$emit('modificaron',result);
-                    $window.location = 'BoxitStore.html#/itemList';
-                }, function error(result) {
-                    console.log(result);
-                });
-            };
-
-            userData.getItemDetails($stateParams.itemId).then(function success(item) {
-                 console.log();
-                if (item != undefined) {
-                    $scope.titulo = item.Item.Attributes.Title;
-                    $scope.texto = getDescription(item).trim();
-                    $scope.imgUrl = item.Item.Image.ImageUrl;
-                    $scope.itemPrice = item.Item.Offers.Offer.OfferListing.Price.FormattedPrice;
-                    amount = item.Item.Offers.Offer.OfferListing.Price.Amount
-                }
-
-                function getDescription(item) {
-                    var description = "";
-                    if (item.Item.Attributes.Feature.length != null && typeof item.Item.Attributes.Feature === "string") {
-                        description = item.Item.Attributes.Feature;
-                    } else {
-                        if (item.Item.Attributes.Feature.length != null) {
-                            for (var i = 0; i < item.Item.Attributes.Feature.length; i++) {
-                                description = description.concat(" ", item.Item.Attributes.Feature[i]);
-                            }
-                        }
-                    }
-                    return description;
-                }
-                userData.getShoppingCar(userData.getData().IdCliente).then(function success(result) {
-                    var carItems = result.data.Data.Cart.CartItems.CartItem;
-                    $scope.cantidad = getQuantity(carItems);
-                    if (amount != undefined && $scope.cantidad != undefined) {
-                        $scope.total = numeral((amount * $scope.cantidad) / 100).format('$0,0.00');
-                    }
-                }, function error(result) {
-                    console.log(result);
-                });
-            }, function error(result) {
-                console.log(result);
-            });
-
-            $scope.refreshTotal = function () {
-                if (amount != undefined) {
-                    $scope.total = numeral((amount * $scope.cantidad) / 100).format('$0,0.00');
-                }
-            }
-*/
+        
         }]);
